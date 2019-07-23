4c69fdfd428f240fee7f7c5d8ec80c9b4e3d158d
Ferenck Vargas <ferenck@acklenavenue.com>
2019-07-23 09:58:01 -0600

    Testing empty user input

---
+++ src/Kata.Spec/when_the_user_input_is_empty.cs
@@ -0,0 +1,16 @@
+using FluentAssertions;
+using Machine.Specifications;
+
+namespace Kata.Spec
+{
+    public class when_the_user_input_is_empty
+    {
+        Establish _context = () => { _systemUnderTest = new Calculator(); };
+
+        Because of = () => { _result = _systemUnderTest.Add(""); };
+
+        It should_return_zero = () => { _result.Should().Be(0); };
+        static Calculator _systemUnderTest;
+        static int _result;
+    }
+}
