cb6a10f97575a3082570f425987298fac95e751d
Byron Sommardahl <byron@acklenavenue.com>
2019-08-09 15:24:08 -0500

    Ping

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 
 namespace Kata
@@ -11,7 +12,8 @@ namespace Kata
                 return 0;
             }
 
-            var array = userInput.Split(",").Select(int.Parse).ToArray();
+            var array = userInput.Split(new []{",", "\n"}, StringSplitOptions.None).Select(int.Parse).ToArray();
+            
 
             return array.Sum();
         }
