d227b4743e3a86ed1ba9716305b336e731ae0d09
Ferenck Vargas <ferenck@acklenavenue.com>
2019-08-13 09:46:25 -0600

    Two numbers passing, unknown amount of numbers failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,12 +1,19 @@
+using System.Linq;
+
 namespace Kata
 {
     public class Calculator
     {
-        public int Add(string s="")
+        public int Add(string userInput="")
         {
-            if (string.IsNullOrEmpty(s))
+            if (string.IsNullOrEmpty(userInput))
                 return 0;
-            return 2;
+            var numbers = userInput.Split(',').Select(int.Parse).ToArray();
+
+            if (numbers.Count() == 1)
+                return numbers.First();
+
+            return numbers.First() + numbers.Last();
         }
     }
 }
