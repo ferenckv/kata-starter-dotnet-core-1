b94628aa8dfdc7f1551d7f6909d8ab0a04c88a31
Byron Sommardahl <byron@acklenavenue.com>
2019-08-13 10:38:38 -0500

    Empty string failing.

--- src/Kata.Spec/when_feeding_the_monkey.cs
+++ src/Kata.Spec/when_feeding_the_monkey.cs
@@ -17,7 +17,21 @@ namespace Kata.Spec
             _systemUnderTest.Belly.Should().Contain("banana");
     }
 
-// 1. Given the user input is empty when calculating the sum then it should return zero.
+    public class when_user_input_is_an_empty_string
+    {
+        Establish _context = () =>
+        {
+            _systemUnderTest = new Calculator();
+        };
+
+        Because of = () => { _result = _systemUnderTest.Add(); };
+
+        It should_return_zero = () => { _result.Should().Be(0); };
+        static Calculator _systemUnderTest;
+        static int _result;
+    }
+
+    // 1. Given the user input is empty when calculating the sum then it should return zero.
 // 2. Given the user input is one number when calculating the sum then it should return the same number. (example "3" should equal 3)
 // 3. Given the user input is two numbers when calculating the sum then it should return the sum of those numbers. (example "1,2" should equal 3)
 // 4. Given the user input is an unknown amount of numbers when calculating the sum then it should return the sum of all the numbers. (example "1,2,3" should equal 6)
