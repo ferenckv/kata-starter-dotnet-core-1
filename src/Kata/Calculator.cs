a6afaefd90035f9691ac681dc18c7da49cb6d7d4
Ferenck Vargas <ferenck@acklenavenue.com>
2019-08-13 09:50:58 -0600

    New line separator passing, custom delimiter failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 
 namespace Kata
@@ -8,7 +9,7 @@ namespace Kata
         {
             if (string.IsNullOrEmpty(userInput))
                 return 0;
-            var numbers = userInput.Split(',').Select(int.Parse).ToArray();
+            var numbers = userInput.Split(new []{",", "\n"}, StringSplitOptions.None).Select(int.Parse).ToArray();
 
             return numbers.Sum();
         }
