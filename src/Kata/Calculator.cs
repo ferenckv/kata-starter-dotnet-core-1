4c332b7f656e9cc1cd17f9e00570060e9f156106
Ferenck Vargas <ferenck@acklenavenue.com>
2019-08-19 09:40:15 -0600

    Two numbers passing, testing for unknown amount of numbers

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+
 namespace Kata
 {
     public class Calculator
@@ -6,7 +8,13 @@ namespace Kata
         {
             if(string.IsNullOrEmpty(s))
                 return 0;
-            return 1;
+
+            var numbers = s.Split(',').Select(int.Parse).ToArray();
+
+            if (numbers.Count() == 1)
+                return numbers.First();
+
+            return numbers.First() + numbers.Last();
         }
     }
 }
