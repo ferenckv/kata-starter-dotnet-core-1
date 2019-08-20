dd8661d53c0c86a52e99e62589a6c0fbc5617331
Ferenck Vargas <ferenck@acklenavenue.com>
2019-08-20 09:41:56 -0600

    Unknow amount of numbers passing, testing for new line as separator

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -9,12 +9,8 @@ namespace Kata
             if (string.IsNullOrEmpty(s))
                 return 0;
 
-            var numbers = s.Split(",").Select(int.Parse);
-            if (numbers.Count() == 1)
-            {
-                return numbers.First();
-            }
-            return numbers.First() + numbers.Last();
+            var numbers = s.Split(",").Select(int.Parse).ToArray();
+            return numbers.Sum();
         }
     }
 }
