cba33a6dd6511c5a7aa13d6ae98719dc5ddd5650
Byron Sommardahl <byron@acklenavenue.com>
2019-09-18 11:58:03 -0500

    large numbers passing, mulpti char delimiter failing.

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -23,7 +23,9 @@ namespace Kata
                 userInput = parts.Last();
             }
             
-            var numbers = userInput.Split(separator, StringSplitOptions.None).Select(int.Parse).ToArray();
+            var numbers = userInput.Split(separator, StringSplitOptions.None).Select(int.Parse)
+                .Where(x=> x<=1000)
+                .ToArray();
 
             var negatives = numbers.Where(x => x < 0);
             if (negatives.Any())
